78c61def64ec3f7d45217a919482373213d9fe54
Matthew Pace <74971601+MatthewPace98@users.noreply.github.com>
2022-10-10 23:06:20 +0100

    Deleted scripts directory

--- scripts/FastQ_Screen.sh
+++
@@ -1,8 +0,0 @@
-a=/home/mpace21/data/FastQ_Screen_out
-
-fastq_screen \
---aligner bowtie2 \
---outdir $a/filtered \
---conf $a/FastQ_Screen_Genomes/fastq_screen.conf \
-/home/mpace21/data/prinseq_out/*good*
-
