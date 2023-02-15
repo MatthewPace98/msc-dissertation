c9d2339cf2480a69fb3a89cfb794b9ac71c3a55c
Matthew Pace <74971601+MatthewPace98@users.noreply.github.com>
2023-02-15 10:30:18 +0000

    Added comments

--- FastQC.sh
+++ FastQC.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env/ bash
 
+# Analyses RNA sequence quality
+
 h=/home/mpace21/data
 
 fastqc \
 -o $h/fastqc/FastQC_filt_out \
 -t 8 \
 /home/mpace21/data/prinseq_out/*good*
-
-# -a $h/FastQC/truseq_adapters.txt \
