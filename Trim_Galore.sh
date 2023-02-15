647552539ba0d0ad4b853e23978cd44d3ef7dcbd
Matthew Pace <74971601+MatthewPace98@users.noreply.github.com>
2023-02-15 10:24:53 +0000

    Added comments

--- Trim_Galore.sh
+++ Trim_Galore.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Trim adapter sequences and low-quality ends of reads using Trim Galore!
+# Discards reads shorter than 45 bp and runs FastQC on trimmed reads
+
 h=/home/mpace21/data
 
 mkdir $h/trim_galore_output
